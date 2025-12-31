2f7fe3627769d6f3e8b92adb05cd9a9963103d94
MOHD <muhammadyacir786@gmail.com>
2025-12-31 19:00:38 +0530

    add miniasidevideolist component for videoplayerpage

---
+++ src/components/Miniasidevideolist.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Miniasidevideolist = () => {
+  return (
+    <div>
+
+    <img src="" alt="" />
+    <div>
+        <h2>title of video</h2>
+        <h3>channel name</h3>
+        <div>
+            <p>views</p>
+            <p>time ago</p>
+        </div>
+
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Miniasidevideolist
