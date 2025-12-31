bc081ce79f3f1f03e250902e2cb9a3f9a6476265
MOHD <muhammadyacir786@gmail.com>
2025-12-31 18:55:02 +0530

    add comments component

---
+++ src/components/Comments.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Comments = () => {
+  return (
+    <div>
+
+    <div><img src="" alt="" /></div>
+    <div>
+        <div>
+        <div>
+            <p>name</p>
+            <p>time</p>
+        </div>
+            <button>edi</button>
+            <button>delete</button>
+        </div>
+        <div>
+            <p>comment</p>
+            <div>
+                <button>like</button>
+                <button>unlike</button>
+            </div>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Comments
